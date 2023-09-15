912ee39dbcc077d02aecc556e0154a551a2db7e1
Bhupendra Singh <bhsingh@gmail.com>
2023-09-15 09:59:20 +0530

    tail itself does chain validation. removed validate functionality

--- commitlog.py
+++ commitlog.py
@@ -99,7 +99,7 @@ class RPC():
 
             return status, meta, await reader.readexactly(length)
         except Exception as e:
-            traceback.print_exc()
+            # traceback.print_exc()
             log(e)
             if self.conns[server][1] is not None:
                 self.conns[server][1].close()
@@ -143,6 +143,9 @@ def get_logfile(log_id, log_seq):
 
 def max_file(log_id):
     logdir = get_logdir(log_id)
+
+    if not os.path.isdir(logdir):
+        return 0, None
 
     # Get max log_seq for this log_id
     # Traverse the three level directory hierarchy picking the highest
@@ -215,19 +218,23 @@ def paxos_server(meta, data):
             return 'OK', [0, 0]
 
         with open(filepath, 'rb') as fd:
-            accepted_seq = json.loads(fd.readline())['accepted_seq']
-            return 'OK', [log_seq, accepted_seq], fd.read()
+            obj = json.loads(fd.readline())
+            md5_chain = obj['md5_chain']
+            accepted_seq = obj['accepted_seq']
+            return 'OK', [log_seq, accepted_seq, md5_chain], fd.read()
 
     if 'accept' == phase and proposal_seq == promised_seq and guid == uuid:
-        log_seq = meta[3]
+        log_seq, md5_chain = meta[3], meta[4]
 
         md5 = hashlib.md5(data).hexdigest()
-        hdr = dict(log_id=log_id, log_seq=log_seq, accepted_seq=proposal_seq,
-                   md5=md5, uuid=uuid, length=len(data))
+        obj = dict(log_id=log_id, log_seq=log_seq, accepted_seq=proposal_seq,
+                   md5=md5, md5_chain=md5_chain, uuid=uuid, length=len(data))
+
+        hdr = json.dumps(obj, sort_keys=True).encode()
 
         dump(get_logfile(log_id, log_seq), hdr, b'\n', data)
 
-        return 'OK', md5
+        return 'OK', hashlib.md5(hdr).hexdigest()
 
     return 'STALE_PROPOSAL_SEQ', proposal_seq
 
@@ -250,8 +257,8 @@ class Client():
             if self.quorum > len(res):
                 return dict(status=False, msec=int((time.time() - ts) * 1000))
 
-            # Format = [[log_seq, accepted_seq], blob]
-            proposal = [[0, 0], b'']
+            # Format = [[log_seq, accepted_seq, md5], blob]
+            proposal = [[0, 0, str(uuid.uuid4())], b'']
             for meta, data in res.values():
                 if meta > proposal[0]:
                     proposal = [meta, data]
@@ -259,9 +266,10 @@ class Client():
             # paxos ACCEPT phase - Write the latest value retrieved earlier
             # Last write from the previous leader might have failed.
             # This client should complete that before any new writes.
-            log_seq, data = proposal[0][0], proposal[1]
+            log_seq, md5, data = proposal[0][0], proposal[0][2], proposal[1]
 
-            meta = [log_id, proposal_seq, guid, proposal[0][0]]
+            # Send old md5 so that an md5 chain can be formed
+            meta = [log_id, proposal_seq, guid, log_seq, md5]
             res = await self.rpc('accept', meta, data)
             if self.quorum > len(res):
                 return dict(status=False, msec=int((time.time() - ts) * 1000))
@@ -269,24 +277,25 @@ class Client():
             md5 = set([meta for meta, data in res.values()])
             if 1 != len(md5):
                 return dict(status=False, msec=int((time.time() - ts) * 1000))
+            md5 = md5.pop()
 
             # This client is THE LEADER now
             # Write successfuly completed. Set next log_seq = log_seq + 1
-            self.logs[log_id] = [proposal_seq, guid, log_seq+1]
+            self.logs[log_id] = [proposal_seq, guid, log_seq+1, md5]
 
             if not blob:
-                return dict(status=True, log_seq=log_seq, md5=md5.pop(),
+                return dict(status=True, log_seq=log_seq, md5=md5,
                             msec=int((time.time() - ts) * 1000))
 
         if not blob:
             return dict(status=True, msec=int((time.time() - ts) * 1000))
 
         # Take away leadership temporarily.
-        proposal_seq, guid, log_seq = self.logs.pop(log_id)
+        proposal_seq, guid, log_seq, md5 = self.logs.pop(log_id)
 
         # Write the blob. Retry a few times to overcome temp failures
         for delay in (1, 1, 1, 1, 1, 0):
-            meta = [log_id, proposal_seq, guid, log_seq]
+            meta = [log_id, proposal_seq, guid, log_seq, md5]
             res = await self.rpc('accept', meta, blob)
 
             if self.quorum > len(res):
@@ -297,37 +306,28 @@ class Client():
                 if 1 != len(md5):
                     await asyncio.sleep(delay)
                     continue
+                md5 = md5.pop()
 
                 # Commit successful. Reinstate as the leader.
-                self.logs[log_id] = [proposal_seq, guid, log_seq+1]
+                self.logs[log_id] = [proposal_seq, guid, log_seq+1, md5]
 
-                return dict(status=True, log_seq=log_seq, md5=md5.pop(),
+                return dict(status=True, log_seq=log_seq, md5=md5,
                             msec=int((time.time() - ts) * 1000))
 
         return dict(status=False, msec=int((time.time() - ts) * 1000))
 
-    async def tail(self, log_id, start_seq, wait_sec=1):
+    async def tail(self, log_id, seq, wait_sec=1):
+        max_seq = seq
+        md5_chain = None
+
         while True:
             res = await self.rpc('logseq', log_id)
-            if self.quorum > len(res):
-                log(f'NO QUORUM start_seq({start_seq})')
-                await asyncio.sleep(wait_sec)
-                continue
+            if len(res) >= self.quorum:
+                max_seq = max([v[0] for v in res.values()])
 
-            max_seq = max([v[0] for v in res.values()])
-            if max_seq <= start_seq:
-                log(f'NO QUORUM start_seq({start_seq}) max_seq({max_seq})')
-                await asyncio.sleep(wait_sec)
-                continue
-
-            for seq in range(start_seq, max_seq):
-                while True:
-                    res = await self.rpc('read', ['meta', log_id, seq])
-                    if self.quorum > len(res):
-                        log(f'NO QUORUM seq({seq}) max_seq({max_seq})')
-                        await asyncio.sleep(wait_sec)
-                        continue
-
+            while seq < max_seq:
+                res = await self.rpc('read', ['meta', log_id, seq])
+                if len(res) >= self.quorum:
                     srv = None
                     accepted_seq = 0
                     for server, res in res.items():
@@ -335,47 +335,34 @@ class Client():
                             srv = server
                             accepted_seq = res[0]['accepted_seq']
 
+                if srv:
                     res = await self.rpc('read', ['data', log_id, seq],
                                          server=srv)
-                    if not res:
-                        log(f'NO QUORUM seq({seq}) max_seq({max_seq})')
-                        await asyncio.sleep(wait_sec)
-                        continue
+                if not res:
+                    log('waiting for quorum')
+                    await asyncio.sleep(wait_sec)
+                    continue
 
-                    yield res[srv]
-                    start_seq = seq + 1
-                    break
+                meta, data = res[srv]
+                if md5_chain and md5_chain != meta['md5_chain']:
+                    log(('chain mismatch', md5_chain, meta['md5_chain']))
+                    return
+
+                hdr = json.dumps(meta, sort_keys=True).encode()
+                md5_chain = hashlib.md5(hdr).hexdigest()
+
+                yield meta, data
+                seq = seq + 1
+
+            log('waiting for quorum or data')
+            await asyncio.sleep(wait_sec)
 
 
 async def command_line_interface():
     logging.basicConfig(format='%(asctime)s %(process)d : %(message)s')
 
-    if 2 == len(sys.argv):
-        # Test code. Validate a log stream after a stress test
-        log_id, log_seq = sys.argv[1], 0
-
-        while True:
-            headers = list()
-            for i in range(1, 6):
-                path = os.path.join(str(i), get_logfile(log_id, log_seq))
-                if os.path.isfile(path):
-                    with open(path, 'rb') as fd:
-                        headers.append(json.loads(fd.readline()))
-
-            if not headers:
-                return
-
-            seq = max([h['accepted_seq'] for h in headers])
-            valid = [h for h in headers if h['accepted_seq'] == seq]
-
-            if len(valid) < 3 or len(set([json.dumps(v) for v in valid])) > 1:
-                return
-
-            log(f'{log_seq} {len(valid)} {valid[0]}')
-            log_seq += 1
-
-    elif 3 == len(sys.argv):
-        # Server
+    # Server
+    if len(sys.argv) < 4:
         cert, port = sys.argv[1], int(sys.argv[2])
 
         SSL = ssl.create_default_context(
