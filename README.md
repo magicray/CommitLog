0f12d00c5c8b7970c8547c34a3c9b3202eb526e5
Bhupendra Singh <bhsingh@gmail.com>
2023-10-12 08:10:17 +0530

    text changes

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ General Purpose Distributed Commit Log - Replicated and Strongly Consistent
 - Leaderless and highly available.
 - Multi Paxos for synchronous and strongly consistent replication.
 - Plain filesystem for persistence.
-- MTLS for auth.
+- HTTP/mTLS interface.
 - Uses only python3 standard library. Zero dependencies.
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
               'Replicated and Strongly Consistent',
   long_description='Leaderless and highly available. '
                    'Multi Paxos for synchronous and consistent replication. '
-                   'Plain filesystem for persistence.',
+                   'Plain filesystem for persistence. HTTP/mTLS interface.',
   author='Bhupendra Singh',
   author_email='bhsingh@gmail.com',
   url='https://github.com/magicray/CommitLog',
